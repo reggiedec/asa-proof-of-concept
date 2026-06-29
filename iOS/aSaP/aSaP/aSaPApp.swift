6feb220425e8361156fdb57e4e77b269fc57308f
dfitzs25 <daltonfitzs25@gmail.com>
2026-06-29 14:21:30 -0400

    basic outline for iOS

---
+++ iOS/aSaP/aSaP/aSaPApp.swift
@@ -0,0 +1,17 @@
+//
+//  aSaPApp.swift
+//  aSaP
+//
+//  Created by Dalton Fitzsimmons on 6/29/26.
+//
+
+import SwiftUI
+
+@main
+struct aSaPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
