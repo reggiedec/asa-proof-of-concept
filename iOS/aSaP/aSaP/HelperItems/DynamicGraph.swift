35f79677d41405998d2d3b2c244a6aa809d9b38b
dfitzs25 <daltonfitzs25@gmail.com>
2026-07-08 14:33:16 -0400

    Addressed comments

--- iOS/aSaP/aSaP/HelperItems/DynamicGraph.swift
+++ iOS/aSaP/aSaP/HelperItems/DynamicGraph.swift
@@ -10,10 +10,11 @@ import Charts
 
 struct DynamicGraph: View {
     private(set) var items: [GraphItem]
+    private(set) var detailsLable: String
     private let cornerRadius: CGFloat = 5
     private let maxGraphHeight: CGFloat = 120
-    private let maxInfoHeight: CGFloat = 50
     @State private var selectedItem: String?
+    @State private var detailSize: CGSize = .init(width: 0, height: 0)
     
     private let identifierColor = Color(
         red: 233/255,
@@ -24,9 +25,11 @@ struct DynamicGraph: View {
     
     @ViewBuilder
     private func detailHeader(for key: String) -> some View {
-        if let selected = items.first(where: { $0.name == key }) {
+        let total = items.filter { $0.name == key }.map(\.value).reduce(0, +)
+        
+        if total > 0 {
             HStack(alignment: .center) {
-                Text("\(String(format: "%.1f", selected.value)) Tons")
+                Text("\(String(format: "%.1f", total)) \(detailsLable)")
                     .font(Font.custom("BeVietnamPro-SemiBold", size: 12))
                     .foregroundStyle(.charcoalBlack)
             }
@@ -34,6 +37,15 @@ struct DynamicGraph: View {
             .background {
                 RoundedRectangle(cornerRadius: 10)
                     .fill(identifierColor)
+            }
+            .background {
+                GeometryReader { geo in
+                    Color.clear
+                        .onAppear { detailSize = geo.size }
+                        .onChange(of: geo.size) {
+                            detailSize = geo.size
+                        }
+                }
             }
             .padding(.horizontal)
         }
@@ -77,6 +89,15 @@ struct DynamicGraph: View {
                         let plot = geo[proxy.plotAreaFrame]
                         let headerX = plot.origin.x + xPos
                         let headerYOffset: CGFloat = 35
+                        let adjustedX = {
+                            let sizeWidth = detailSize.width
+                            if headerX + (sizeWidth / 2) > geo.size.width {
+                                // too big, apearing over the edge
+                                return geo.size.width - (sizeWidth / 2)
+                            } else {
+                                return headerX
+                            }
+                        }
                         
                         ZStack {
                             RoundedRectangle(cornerRadius: 0)
@@ -90,7 +111,7 @@ struct DynamicGraph: View {
                                 .position(x: headerX, y: plot.origin.y - headerYOffset / 2)
                             
                             detailHeader(for: key)
-                                .position(x: headerX, y: plot.origin.y - headerYOffset)
+                                .position(x: adjustedX(), y: plot.origin.y - headerYOffset)
                         }
                     }
                 }
@@ -106,9 +127,12 @@ struct DynamicGraph: View {
         .init(name: "C", value: 15, fill: LinearGradient(colors: [.blueGradient.opacity(0.7), .blueGradient, .blueGradient], startPoint: .top, endPoint: .bottom)),
         .init(name: "D", value: 15, fill: Color.grayBars),
         .init(name: "E", value: 15, fill: LinearGradient(colors: [.blueGradient.opacity(0.7), .blueGradient, .blueGradient], startPoint: .top, endPoint: .bottom)),
-        .init(name: "F", value: 15, fill: Color.grayBars),
+        .init(name: "F", value: 13, fill: Color.grayBars),
+        .init(name: "F", value: 14, fill: Color.red),
+        .init(name: "F", value: 105, fill: Color.blue),
         
         .init(name: "G", value: 15, fill: LinearGradient(colors: [.blueGradient.opacity(0.7), .blueGradient, .blueGradient], startPoint: .top, endPoint: .bottom)),
-    ])
+    ], detailsLable: "Buns"
+    )
 }
 
